f0c293564963053d116d25d4a5dd424d6b3a8c7f
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2023-11-01 04:08:00 +0300

    делаю проверку кнопок

--- Assets/Scripts/CheckTrueButtons.cs
+++ Assets/Scripts/CheckTrueButtons.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class CheckTrueButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject[] _buttons;
+    [SerializeField] private int _countAnswers;
 
-    // Update is called once per frame
-    void Update()
+    public void CheckAnswers()
     {
-        
+        if (_buttons[0].GetComponent<Image>().sprite.name == "clickedBtn")
+        {
+            if (_buttons[1].GetComponent<Image>().sprite.name == "clickedBtn")
+            {
+                if (_buttons[2].GetComponent<Image>().sprite.name == "clickedBtn")
+                {
+                    if (_buttons[3].GetComponent<Image>().sprite.name == "clickedBtn")
+                    {
+                        if (_buttons[4].GetComponent<Image>().sprite.name == "clickedBtn")
+                        {
+                            print("ok");
+                        }
+                    }
+                }
+            }
+        }
+           
     }
 }
